c1190de736b1955b4de513635ebfb767f7357c4a
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-11 09:00:55 -0500

    first commit

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(){
+    return (
+      <ul>
+        <li>Seaweed</li>
+        <li>Sauteed onions</li>
+        <li>Sourdough bread</li>
+        <li>Roasted vegetables</li>
+      </ul>
+    );
+  }
+
+  export default List;
